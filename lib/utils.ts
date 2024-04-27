402afb39a9a4bfd04291d2ea4c3ca39755bcd714
nqhung291 <nqhung291@gmail.com>
2024-04-27 16:00:05 +0700

    feat: add components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const cn = (...classes: ClassValue[]) => {
-  return twMerge(clsx(classes))
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
